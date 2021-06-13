1c828aa2d2e608db0ba16b5cf6128b58ad36d0b0
durden <therogerdeas@hotmail.com>
2021-06-13 14:17:05 -0500

    new changes for cloud

--- src/main/java/com/api/apis/ApisApplication.java
+++ src/main/java/com/api/apis/ApisApplication.java
@@ -2,8 +2,10 @@ package com.api.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApisApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/api/apis/controllers/ParamQueryController.java
@@ -0,0 +1,13 @@
+package com.api.apis.controllers;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping(value = "/pq")
+public class ParamQueryController {
+
+}
